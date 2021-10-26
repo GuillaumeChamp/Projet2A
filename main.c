ac81173c423518d1884d89e778ed1f7685ec256d
Cycytrcht <79595098+Cycytrcht@users.noreply.github.com>
2021-10-26 17:23:07 +0200

    make LED blink

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include "main.h"
 #include "decisionMaker.h"
+#include <stdio.h>
+#include <wiringPi.h>
 
-void main(){
-  while(1)
+#define LED     1
+
+int main (void)
+{
+  printf ("Raspberry Pi Quick2Wire blink\n") ;
+
+  wiringPiSetup () ;
+  pinMode (LED, OUTPUT) ;
+
+  for (;;)
+  {
+    digitalWrite (LED, HIGH) ;  // On
+    delay (500) ;               // mS
+    digitalWrite (LED, LOW) ;   // Off
+    delay (500) ;
+  }
+  return 0 ;
 }
