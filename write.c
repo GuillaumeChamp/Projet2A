b9ccf0a3faa1f6cf2e2ff907caf9be1e3fbaa701
GuillaumeChamp <80750657+GuillaumeChamp@users.noreply.github.com>
2021-11-23 14:57:00 +0100

    Update write.c

--- write.c
+++ write.c
@@ -1,9 +1,16 @@
-
 #include <wiringSerial.h>
+#include <wiringPiI2C.h>
 
 int send(char c){
         int filedescriptor;
-        if((filedescriptor = serialOpen("/dev/ttyAMA0",230400)) < 0) return 1;
+        char* port = "/dev/serial0";
+        if((filedescriptor = serialOpen(port,110)) < 0) return 1;
         serialPutchar(filedescriptor,c);
+        serialClose(filedescriptor);
+        return 0;
+}
+int I2Csend(int c){
+        int filedescriptor = wiringPiI2CSetup(0x20);
+        wiringPiI2CWrite(filedescriptor,c);
         return 0;
 }
