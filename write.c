075e4ea66a8fe12b31ab66ab706b4b78eab64266
GuillaumeChamp <80750657+GuillaumeChamp@users.noreply.github.com>
2021-11-23 17:01:29 +0100

    Update write.c

--- write.c
+++ write.c
@@ -4,7 +4,7 @@
 int send(char c){
         int filedescriptor;
         char* port = "/dev/serial0";
-        if((filedescriptor = serialOpen(port,110)) < 0) return 1;
+        if((filedescriptor = serialOpen(port,600)) < 0) return 1;
         serialPutchar(filedescriptor,c);
         serialClose(filedescriptor);
         return 0;
@@ -13,4 +13,12 @@ int I2Csend(int c){
         int filedescriptor = wiringPiI2CSetup(0x20);
         wiringPiI2CWrite(filedescriptor,c);
         return 0;
+}
+char read(){
+        int fd;
+        char* port = "/dev/serial0";
+        if ((fd = serialOpen(port,600))<0) return 1;
+        char ans = serialGetchar(fd);
+        serialClose(fd);
+        return ans;
 }
