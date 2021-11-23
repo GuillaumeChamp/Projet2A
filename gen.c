7738c983c9782350b327e82171338aa2f449c157
GuillaumeChamp <80750657+GuillaumeChamp@users.noreply.github.com>
2021-11-23 16:59:45 +0100

    Update gen.c

--- gen.c
+++ gen.c
@@ -1,10 +1,10 @@
-
 #include <wiringPi.h>
 #include <stdio.h>
 #include "write.h"
+
 PI_THREAD (SendData)
 {
-        piHiPri(20);
+        piHiPri(8);
         while(1){
                 send('8');
                 send('e');
@@ -12,17 +12,26 @@ PI_THREAD (SendData)
         }
 }
 
+PI_THREAD (SendDataI2C)
+{
+        piHiPri(25);
+        while(1){
+                I2Csend(1000);
+                I2Csend(400);
+                I2Csend(20);
+        }
+}
+
 int main (void)
 {
         wiringPiSetup();
         int i = piHiPri(10);
-        int x  = piThreadCreate(SendData);
-        printf("started p = %d %d\n",i,x);
+        int x = piThreadCreate(SendData);
+        //int y = piThreadCreate(SendDataI2C);
+        printf("started p = %d ,thread : %d \n",i,x);
         while(1)
         {
-
+                printf("In = %c \n",read());
         }
         return 0;
-
 }
-
