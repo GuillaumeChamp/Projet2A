623d684cd75cff7bb1d7ffb16a6abb2c318cc161
Cycytrcht <79595098+Cycytrcht@users.noreply.github.com>
2022-02-08 16:17:03 +0100

    update I2CRead_test.c for free running mode

--- rpi1/I2CRead_test.c
+++ rpi1/I2CRead_test.c
@@ -9,9 +9,11 @@
 #include <wiringPiI2C.h>
 #include "UARTI2Cfun.h"
 
-#define LIDAR_I2C_ADDR 0x62
-#define LIDAR_CMD_REG 0x00*
-#define LIDAR_MODE 0x04
+#define LIDAR_I2C_ADDR  0x62
+#define LIDAR_CMD_REG   0x00
+#define LIDAR_MODE      0x04
+#define LIDAR_FRM_REG   0x11 // Free Running Mode
+#define LIDAR_FRM       0xFF
 
 int main(void) {
   int fd = wiringPiI2CSetup(LIDAR_I2C_ADDR);
@@ -19,14 +21,22 @@ int main(void) {
     printf("Setup problem\n");
     exit(EXIT_FAILURE);
   }
+  
+  int frm_activation = wiringPiI2CWriteReg8(fd,LIDAR_FRM_REG,LIDAR_FRM);
+  if (frm_activation == -1) {
+    printf("Problem with setting up free running mode\n");
+    exit(EXIT_FAILURE);
+  }
+  
   int askMeasure = wiringPiI2CWriteReg8(fd,LIDAR_CMD_REG,LIDAR_MODE);
   if (askMeasure == -1) {
     printf("Problem with measure asking\n");
     exit(EXIT_FAILURE);
   }
+  
   int new_measure;
   while(1) {
-    askMeasure = wiringPiI2CWriteReg8(fd,LIDAR_CMD_REG,LIDAR_MODE);
+//    askMeasure = wiringPiI2CWriteReg8(fd,LIDAR_CMD_REG,LIDAR_MODE); // line to add if no in free running mode
     new_measure = readI2C(fd);
     printf("I2C read = %d\n", new_measure);
     sleep(1);
