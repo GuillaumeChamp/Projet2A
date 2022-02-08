002984b4f7455a63c01832e62af271ecc1b8ed74
Cycytrcht <79595098+Cycytrcht@users.noreply.github.com>
2022-02-08 16:22:56 +0100

    update UARTI2Cfun.c for free running mode

--- rpi1/UARTI2Cfun.c
+++ rpi1/UARTI2Cfun.c
@@ -5,9 +5,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define LIDAR_LOWBYTE_REG 0x10
-#define LIDAR_HIGHBYTE_REG 0x0f
-#define LIDAR_STATUS_REG 0x01
+#define LIDAR_LOWBYTE_REG   0x10
+#define LIDAR_HIGHBYTE_REG  0x0f
+#define LIDAR_STATUS_REG    0x01
 
 int sendUART(char c) {
   int fd;
@@ -37,18 +37,20 @@ char readUART() {
 }
 
 int readI2C(int fd) {
-  int busy = wiringPiI2CReadReg8(fd,LIDAR_STATUS_REG)&0x01;
+/*  int busy = wiringPiI2CReadReg8(fd,LIDAR_STATUS_REG)&0x01;
   while(busy) {
     printf("Busy\n");
     busy =wiringPiI2CReadReg8(fd,LIDAR_STATUS_REG)&0x01;
   }
   printf("Not busy\n");
+*/
+  // bloc to add if not in free running mode
   
-  if (wiringPiI2CReadReg8(fd,LIDAR_STATUS_REG)&0b01000000) == 0b01000000) {
+if (wiringPiI2CReadReg8(fd,LIDAR_STATUS_REG)&0b01000000) == 0b01000000) {
     printf("System error during measurement\n");
     exit(EXIT_FAILURE);
   }
-  
+
   int lowbyte = wiringPiI2CReadReg8(fd,LIDAR_LOWBYTE_REG);
   int highbyte = wiringPiI2CReadReg8(fd,LIDAR_HIGHBYTE_REG);
   if (lowbyte == -1) {printf("Lowbyte reading error\n"); exit(EXIT_FAILURE);}
@@ -56,20 +58,4 @@ int readI2C(int fd) {
   
   int ans = lowbyte + 256*highbyte;
   return ans;
-}
-
-int readUARTv2() { //formerly in read.c file
-  int fd;
-  char ch;
-  if (wiringPiSetup()<0) {return 0;}
-  if ((fd=serialOpen("/dev/serial1",110))<0) {return 1;}
-  while(1) {
-    if (serailDataAvail(fd)>0) {
-      ch = serialGetchar(fd);
-      /*if (ch!=0)*/ printf("%c\n",ch);
-      printf("while loop\n");
-    }
-  }
-  serialClose(fd);
-  return 0;
 }
