b050ce9fba504430b8e91c38d601af8d64f64650
Guillaume Champtoussel <80750657+GuillaumeChamp@users.noreply.github.com>
2022-02-08 16:28:18 +0100

    Update UARTI2Cfun.c

--- rpi1/UARTI2Cfun.c
+++ rpi1/UARTI2Cfun.c
@@ -28,9 +28,7 @@ int sendI2C(int c) {
 char readUART() {
   int fd;
   char* port = "/dev/serial0";
-  if ((fd=serialOpen(port,1152000) < 0) {
-    return 1;
-  }
+  if ((fd=serialOpen(port,115200) < 0) return -1;
   char ans = serialGetchar(fd);
   serialClose(fd);
   return ans;
