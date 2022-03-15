f7024f10c488a1e9b81a1c8b5e226828045e77c8
Cycytrcht <79595098+Cycytrcht@users.noreply.github.com>
2022-03-15 15:54:05 +0100

    add IMU config in main.c

--- rpi1/main.c
+++ rpi1/main.c
@@ -19,7 +19,9 @@
 
 extern int errno;
 
-double uartResult = 0;
+long xacc; // Float 32 : 4 bytes long single precision floating point
+long yacc;
+long zacc; 
 double i2cResult = 0;
 double uartI2Csum = 0;
 
@@ -27,26 +29,96 @@ int UART_received = 0;
 
 PI_THREAD (ReadUart){
         piHiPri(15);
-        while(1){
-                double data = 0;
-                int dot = 0;
-                int nb = 0;
-                char read;
-                while ((read=readUART()) !='\n' && nb < 7){
-                        if (read == '.') dot = nb;
-                        else if (read>47) {
-                                nb++;
-                                data = data*10 + read;
-                        }
-                }
-                UART_received = 1;
-                // printf("UART_received (uart) : %d\n",UART_received);
-                data = data/pow(10,nb+1-dot);
-                uartResult = data;
-                printf("uartResult : %f\n",uartResult);
-                uartI2Csum = uartResult + i2cResult;
-                printf("SUM = %f\n", uartI2Csum);
+        
+        // initialisation of the rpi port
+        int fd;
+        char* port = "/dev/serial0";
+        if ((fd = serialOpen(port, 115200)) < 0) {
+                printf("Problem opening UART port\n"); 
+                exit(EXIT_FAILURE);
         }
+                
+        int buffLen = 50;
+        char rdBuffer[buffLen]; // will receive the values read from the MT
+        char wrBuffer[buffLen]; // will contain the values to be send to the MT
+
+        int configDone = 0;
+        while (!configDone) {
+                
+                memset(rdBuffer, 0, buffLen*sizeof(char));
+                memset(wrBuffer, 0, buffLen*sizeof(char));
+                
+                // receiving data
+                if (serialDataAvail(fd) == -1) {
+                        printf("Problem reading if data available\n");
+                        exit(EXIT_FAILURE);
+                } 
+                while (serialDataAvail(fd) == 0) {
+                        printf("No data available\n");
+                }
+                int rdCount = 0;
+                while (serialDataAvail(fd) > 0) {
+                        rdBuffer[rdCount] = serialGetchar(fd);
+                        rdCount++;
+                }
+                
+                // interpreting received data and answer in consequence
+                if ((rdBuffer[4] == '3')&&(rdBuffer[5] == 'E')) { // WakeUp signal
+                        wrBuffer[0] = 'FAFF3000D1'; // GoToConfig signal
+                        serialPuts(wrBuffer);
+                }
+                
+                if ((rdBuffer[4] == '3')&&(rdBuffer[5] == '1')) { // GoToConfigAck signal
+                        wrBuffer[0] = 'FAFF18010200'; // SetBaudrate signal
+                        serialPuts(wrBuffer);
+                } 
+                
+                if ((rdBuffer[4] == '1')&&(rdBuffer[5] == '9')) { // SetBaudrateAck signal
+                        wrBuffer[0] = 'FAFFC0024020006401'; // SetOutputConfiguration signal
+                        serialPuts(wrBuffer);
+                }
+                
+                if ((rdBuffer[4] == 'C')&&(rdBuffer[5] == '0')) { // SetOutputConfigurationAck signal
+                        wrBuffer[0] = 'FAFF100001'; // GoToMeasurement signal
+                        serialPuts(wrBuffer);
+                }
+                
+                if ((rdBuffer[4] == '1')&&(rdBuffer[5] == '1')) { // GoToMeasurementAck signal
+                        configDone = 1;
+                }
+        
+        }
+        
+        while(1){ // read MTData2 
+                
+                memset(rdBuffer, 0, buffLen*sizeof(char));
+                
+                // receiving data
+                while (serialDataAvail(fd) == -1) {
+                        printf("No data available\n");
+                }
+                int rdCount = 0;
+                while (serialDataAvail(fd) > -1) {
+                        rdBuffer[rdCount] = serialGetchar(fd);
+                        rdCount++;
+                }
+                
+                if ((rdBuffer[4] == '3')&&(rdBuffer[5] == '6')) { // MTData
+                        char tempxacc[4];
+                        char tempyacc[4];
+                        char tempzacc[4];
+                        char *eptr;
+                        strncpy(tempxacc,rdBuffer[16],4*sizeof(char));
+                        strncpy(tempyacc,rdBuffer[20],4*sizeof(char));
+                        strncpy(tempzacc,rdBuffer[24],4*sizeof(char));
+                        xacc = strtol(tempxacc, &eptr, 4);
+                        yacc = strtol(tempyacc, &eptr, 4);
+                        zacc = strtol(tempzacc, &eptr, 4);
+                }
+                printf(" xacc : %ld\n yacc : %ld\n zacc : %ld\n",xacc,yacc,zacc);
+        }
+        
+        serialClose(fd);
 }
 
 PI_THREAD (ReadI2C){
